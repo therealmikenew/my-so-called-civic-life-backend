73b0d8f53e2be263cb4f5e9bd651ce831fa6f496
Michael Newman <therealmichaelnewman@gmail.com>
2022-02-07 14:52:05 -0500

    make migration files

--- civic_life_django/civic_life/models.py
+++ civic_life_django/civic_life/models.py
@@ -1,5 +1,3 @@
-from operator import mod
-from sre_parse import State
 from django.db import models
 
 # Create your models here.
@@ -16,3 +14,31 @@ class User(models.Model):
 
     def __str__(self):
         return self.first_name
+
+
+class Legislation(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='legislations')
+    title = models.TextField(blank=True)
+    bill_number = models.CharField(max_length=50)
+    summary = models.TextField(blank=True)
+    url = models.TextField(blank=True)
+    sponsor = models.CharField(max_length=200, default="no sponsor listed")
+    cosponsor = models.CharField(
+        max_length=200, default="no co-sponsor listed")
+    date_introduced = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.bill_number
+
+
+class Action(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='actions')
+    date = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.date
