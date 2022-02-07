33dedf6cf0e0d91a87a77072345ceaff08c09d74
Michael Newman <therealmichaelnewman@gmail.com>
2022-02-07 15:17:26 -0500

    register models; create superuser

--- civic_life_django/civic_life/admin.py
+++ civic_life_django/civic_life/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Legislation, Action
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Legislation)
+admin.site.register(Action)
