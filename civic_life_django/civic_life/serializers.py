93aa08912b8bca17292b690746396b9464ef2651
Michael Newman <therealmichaelnewman@gmail.com>
2022-02-08 05:50:44 -0500

    update serializers.py and urls.py

--- civic_life_django/civic_life/serializers.py
+++ civic_life_django/civic_life/serializers.py
@@ -14,10 +14,14 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True
     )
 
+    user_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='user_detail'
+    )
+
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'profile_URL',
-                  'street_address', 'city', 'state', 'zip_code', 'legislations', 'actions')
+                  'street_address', 'city', 'state', 'zip_code', 'legislations', 'actions', 'user_url')
 
 
 class LegislationSerializer(serializers.HyperlinkedModelSerializer):
@@ -30,10 +34,14 @@ class LegislationSerializer(serializers.HyperlinkedModelSerializer):
         queryset=User.objects.all(), source='user'
     )
 
+    legislation_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='legislation_detail'
+    )
+
     class Meta:
         model = Legislation
         fields = ('id', 'title', 'bill_number', 'summary', 'url',
-                  'sponsor', 'cosponsor', 'date_introduced', 'user', 'user_id')
+                  'sponsor', 'cosponsor', 'date_introduced', 'user', 'user_id', 'legislation_url')
 
 
 class ActionSerializer(serializers.HyperlinkedModelSerializer):
@@ -46,7 +54,11 @@ class ActionSerializer(serializers.HyperlinkedModelSerializer):
         queryset=User.objects.all(), source='user'
     )
 
+    action_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='action_detail'
+    )
+
     class Meta:
         model = Action
         fields = ('id', 'date', 'description', 'notes',
-                  'completed', 'user', 'user_id')
+                  'completed', 'user', 'user_id', 'action_url')
--- civic_life_django/civic_life/urls.py
+++ civic_life_django/civic_life/urls.py
@@ -4,8 +4,11 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('user/', views.UserList.as_view(), name='user'),
+    path('legislation/', views.LegislationList.as_view(), name='legislation'),
     path('legislation/<int:pk>', views.LegislationDetail.as_view(),
          name='legislation_detail'),
+    path('action/', views.ActionList.as_view(), name='action'),
+
     path('action/<int:pk>', views.ActionDetail.as_view(),
          name='action_detail'),
     path('user/<int:pk>', views.UserList.as_view(), name='user_detail')
