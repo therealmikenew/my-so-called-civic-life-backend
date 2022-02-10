1f1713e06e086c04ac205636247ed2c849182289
Michael Newman <therealmichaelnewman@gmail.com>
2022-02-10 13:46:28 -0500

    update settings.py

--- civic_life_django/civic_life_django/settings.py
+++ civic_life_django/civic_life_django/settings.py
@@ -58,12 +58,20 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.domain\.com$",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://domain.com",
     "https://api.domain.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000"
+    "http://localhost:8000",
+    "http://127.0.0.1:9000",
+    "https://civicinfo.googleapis.com"
 ]
+
+
+# CORS_ORIGIN_WHITELIST = ["https://civicinfo.googleapis.com"]
+
 
 CORS_ALLOW_METHODS = [
     'DELETE',
